dfb7620bedf0ec7c425ac98d0626c5c73a6f8b47
jussisaurio <jussi.saurio@gmail.com>
2021-04-25 12:06:00 +0300

    Add JSON tokenizer and JSON parser

--- app/JSON.hs
+++ app/JSON.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
@@ -12,10 +13,14 @@
 
 module JSON where
 
+import Control.Applicative (Alternative ((<|>)))
 import Data.List (intercalate)
+import Data.Maybe (catMaybes)
 import qualified Data.Text as T
 import GHC.Generics
+import Parser
 
+-- Serializer
 class ToJSON (a :: *) where
   toJSON :: a -> String
   default toJSON :: (Generic a, GenericToJSON (Rep a)) => a -> String
@@ -58,3 +63,118 @@ instance (GenericToJSON constructorLeft, GenericToJSON constructorRight) => Gene
 
 instance GenericToJSON constructors => GenericToJSON (M1 D d constructors) where
   genericToJSON x = let constructors = unM1 x in genericToJSON constructors
+
+-- JSON Tokenizer
+
+data JSONToken = ObjectStart | ObjectEnd | ArrayStart | ArrayEnd | Colon | Comma | JString String | JNumber Double | JNull | JTrue | JFalse deriving (Eq, Show)
+
+newline :: Parser Char (Maybe a)
+newline = Nothing <$ (prefix "\r\n" <|> prefix "\n")
+
+jspace :: Parser Char (Maybe a)
+jspace = Nothing <$ space
+
+objectStart :: Parser Char (Maybe JSONToken)
+objectStart = Just ObjectStart <$ char '{'
+
+objectEnd :: Parser Char (Maybe JSONToken)
+objectEnd = Just ObjectEnd <$ char '}'
+
+arrayStart :: Parser Char (Maybe JSONToken)
+arrayStart = Just ArrayStart <$ char '['
+
+arrayEnd :: Parser Char (Maybe JSONToken)
+arrayEnd = Just ArrayEnd <$ char ']'
+
+colon :: Parser Char (Maybe JSONToken)
+colon = Just Colon <$ char ':'
+
+comma = Just Comma <$ char ','
+
+stringToken :: Parser Char (Maybe JSONToken)
+stringToken = Just . JString <$> (prefix "\"" *> zeroOrMore (anythingBut '"') <* prefix "\"")
+
+numberToken :: Parser Char (Maybe JSONToken)
+numberToken = Just . JNumber . read <$> ((++) <$> intParser <*> (concat <$> maybeOne ((++) <$> prefix "." <*> posIntParser)))
+
+jnull :: Parser Char (Maybe JSONToken)
+jnull = Just JNull <$ prefix "null"
+
+jbool :: Parser Char (Maybe JSONToken)
+jbool = Just JTrue <$ prefix "true" <|> Just JFalse <$ prefix "false"
+
+tokenizer :: Parser Char [Maybe JSONToken]
+tokenizer = oneOrMore (newline <|> jspace <|> objectStart <|> objectEnd <|> arrayStart <|> arrayEnd <|> colon <|> comma <|> stringToken <|> numberToken <|> jnull <|> jbool)
+
+tokenize :: [Char] -> Either String [JSONToken]
+tokenize input = case runParser tokenizer input of
+  Nothing -> Left "tokenizer error"
+  Just (tokenMaybes, "") -> Right (catMaybes tokenMaybes)
+  Just (_, remainder) -> Left ("tokenizer error: remainder is " <> remainder)
+
+-- JSON Parser
+
+newtype JSONKey = JSONKey String deriving (Eq, Show)
+
+data JSON = JSONString String | JSONNumber Double | JSONBoolean Bool | JSONNull | JSONArray [JSON] | JSONObject [(JSONKey, JSON)] deriving (Eq, Show)
+
+parseNull :: Parser JSONToken JSON
+parseNull = JSONNull <$ satisfy (JNull ==)
+
+parseBool :: Parser JSONToken JSON
+parseBool = JSONBoolean True <$ satisfy (JTrue ==) <|> JSONBoolean False <$ satisfy (JFalse ==)
+
+parseString :: Parser JSONToken JSON
+parseString =
+  JSONString . (\(JString x) -> x)
+    <$> satisfy
+      ( \case
+          JString _ -> True
+          _ -> False
+      )
+
+parseNumber :: Parser JSONToken JSON
+parseNumber =
+  JSONNumber . (\(JNumber x) -> x)
+    <$> satisfy
+      ( \case
+          JNumber _ -> True
+          _ -> False
+      )
+
+parseKey :: Parser JSONToken JSONKey
+parseKey =
+  JSONKey . (\(JString x) -> x)
+    <$> satisfy
+      ( \case
+          JString _ -> True
+          _ -> False
+      )
+      <* satisfy (== Colon)
+
+parseLiteral :: Parser JSONToken JSON
+parseLiteral = parseNull <|> parseBool <|> parseString <|> parseNumber
+
+parseJSON :: Parser JSONToken JSON
+parseJSON = parseLiteral <|> parseJSONArray <|> parseObject
+
+parseArray :: Parser JSONToken [JSON]
+parseArray = (++) <$> zeroOrMore (parseJSON <* satisfy (== Comma)) <*> maybeOne parseJSON
+
+parseJSONArray :: Parser JSONToken JSON
+parseJSONArray = JSONArray <$> (satisfy (== ArrayStart) *> parseArray <* satisfy (== ArrayEnd))
+
+parseKVPair :: Parser JSONToken (JSONKey, JSON)
+parseKVPair = (,) <$> parseKey <*> parseJSON
+
+parseEntries :: Parser JSONToken [(JSONKey, JSON)]
+parseEntries = (++) <$> zeroOrMore (parseKVPair <* satisfy (== Comma)) <*> maybeOne parseKVPair
+
+parseObject :: Parser JSONToken JSON
+parseObject = JSONObject <$> (satisfy (== ObjectStart) *> parseEntries <* satisfy (== ObjectEnd))
+
+jsonParse :: [JSONToken] -> Either String JSON
+jsonParse tokens = case runParser parseJSON tokens of
+  Nothing -> Left "parse error"
+  Just (json, []) -> Right json
+  Just (_, remainder) -> Left ("parse error: remainder is " <> show remainder)
---
+++ app/Parser.hs
@@ -0,0 +1,110 @@
+module Parser where
+
+import Control.Applicative
+import Data.Char (isAlpha, isDigit, isSpace)
+import Data.List
+import Text.Read
+
+newtype Parser a t = Parser {runParser :: [a] -> Maybe (t, [a])}
+
+satisfy :: (Eq a) => (a -> Bool) -> Parser a a
+satisfy predicate = Parser func
+  where
+    func [] = Nothing -- fail on empty input
+    func (x : xs)
+      | predicate x = Just (x, xs)
+      | otherwise = Nothing
+
+prefix :: Eq a => [a] -> Parser a [a]
+prefix str = Parser func
+  where
+    func x = if str `isPrefixOf` x then Just (take (length str) x, drop (length str) x) else Nothing
+
+letter :: Parser Char Char
+letter = satisfy isAlpha
+
+digit :: Parser Char Char
+digit = satisfy isDigit
+
+char :: Char -> Parser Char Char
+char = satisfy . (==)
+
+times :: Integer -> Parser t a -> Parser t [a]
+times x p = if x <= 1 then (: []) <$> p else (:) <$> p <*> times (x - 1) p
+
+anyOf :: (Eq a, Foldable t) => t a -> Parser a a
+anyOf chars = satisfy $ flip elem chars
+
+anythingBut :: (Eq a, Show a) => a -> Parser a a
+anythingBut = satisfy . (/=)
+
+nothing :: Parser a ()
+nothing = Parser $ \tokens -> if null tokens then Just ((), tokens) else Nothing
+
+first :: (a -> b) -> Parser t a -> Parser t b
+first mapper (Parser runParserInstance1) = Parser runParserInstance2
+  where
+    runParserInstance2 inputString = case runParserInstance1 inputString of
+      Nothing -> Nothing
+      Just (x, xs) -> Just (mapper x, xs)
+
+instance Functor (Parser a) where
+  fmap = first
+
+instance Alternative (Parser a) where
+  empty = Parser (const Nothing)
+  (<|>) (Parser f1) (Parser f2) = Parser f3
+    where
+      f3 inputString = f1 inputString <|> f2 inputString
+
+pureParser :: a -> Parser t a
+pureParser val = Parser (\x -> Just (val, x))
+
+parserApply :: (a -> b -> c) -> Parser t a -> Parser t b -> Parser t c
+parserApply mapper (Parser f1) (Parser f2) = Parser f3
+  where
+    f3 inputString = do
+      (val1, rest1) <- f1 inputString
+      (val2, rest2) <- f2 rest1
+      pure (mapper val1 val2, rest2)
+
+instance Applicative (Parser a) where
+  pure = pureParser
+  (<*>) = parserApply id
+
+zeroOrMore :: Parser t a -> Parser t [a]
+zeroOrMore p = oneOrMore p <|> pure []
+
+oneOrMore :: Parser t a -> Parser t [a]
+oneOrMore p = (:) <$> p <*> zeroOrMore p
+
+maybeOne :: Parser a1 a2 -> Parser a1 [a2]
+maybeOne p = Parser func
+  where
+    func inp = case runParser p inp of
+      Nothing -> Just ([], inp)
+      Just (x, xs) -> Just ([x], xs)
+
+eatWhitespace :: Parser Char String
+eatWhitespace = zeroOrMore (satisfy isSpace)
+
+space :: Parser Char [Char]
+space = oneOrMore (satisfy isSpace)
+
+intParser = Parser f
+  where
+    f inputString
+      | null numberlikeString = Nothing
+      | otherwise = Just (numberlikeString, rest)
+      where
+        (numberlikeString, rest) = case inputString of
+          '-' : xs -> let (r, rs) = span isDigit xs in ('-' : r, rs)
+          xs -> span isDigit xs
+
+posIntParser = Parser f
+  where
+    f inputString
+      | null numberlikeString = Nothing
+      | otherwise = Just (numberlikeString, rest)
+      where
+        (numberlikeString, rest) = span isDigit inputString
