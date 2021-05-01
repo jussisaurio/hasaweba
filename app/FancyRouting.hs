e8f87e50a42aaa22d2f59eb01e8b161e9749a813
jussisaurio <jussi.saurio@gmail.com>
2021-05-01 14:01:28 +0300

    make FancyRouting type variables a bit easier to understand

--- app/FancyRouting.hs
+++ app/FancyRouting.hs
@@ -54,38 +54,42 @@ type instance Server (Capture a :> r) = a -> Server r
 
 type instance Server (BodyParser a :> r) = a -> Server r
 
-class HasServer layout a z where
-  route :: Proxy layout -> Server layout -> (T.Text, [T.Text], z) -> Maybe (AppCtx a)
+type Method = T.Text
 
-instance (ToJSON a) => HasServer (Respond a) JSON z where
+type PathParts = [T.Text]
+
+class HasServer layout returnT body where
+  route :: Proxy layout -> Server layout -> (Method, PathParts, body) -> Maybe (AppCtx returnT)
+
+instance (ToJSON a) => HasServer (Respond a) JSON body where
   route _ handler (_, [], _) = Just (toJSON <$> handler)
   route _ _ _ = Nothing
 
-instance (HasServer a x z, HasServer b x z) => HasServer (a :<|> b) x z where
-  route :: Proxy (a :<|> b) -> (Server a :<|> Server b) -> (T.Text, [T.Text], z) -> Maybe (AppCtx x)
+instance (HasServer a returnT body, HasServer b returnT body) => HasServer (a :<|> b) returnT body where
+  route :: Proxy (a :<|> b) -> (Server a :<|> Server b) -> (Method, PathParts, body) -> Maybe (AppCtx returnT)
   route _ (handlera :<|> handlerb) req = route (Proxy :: Proxy a) handlera req <|> route (Proxy :: Proxy b) handlerb req
 
 -- TODO: figure out how to deduplicate the two instances below:
-instance (HasServer s x z) => HasServer (GET :> s) x z where
-  route :: Proxy (a :> s) -> Server s -> (T.Text, [T.Text], z) -> Maybe (AppCtx x)
+instance (HasServer s returnT body) => HasServer (GET :> s) returnT body where
+  route :: Proxy (a :> s) -> Server s -> (Method, PathParts, body) -> Maybe (AppCtx returnT)
   route _ handler (method, xs, z) = if "GET" == method then route (Proxy :: Proxy s) handler (method, xs, z) else Nothing
 
-instance (HasServer s x z) => HasServer (POST :> s) x z where
-  route :: Proxy (a :> s) -> Server s -> (T.Text, [T.Text], z) -> Maybe (AppCtx x)
+instance (HasServer s returnT body) => HasServer (POST :> s) returnT body where
+  route :: Proxy (a :> s) -> Server s -> (Method, PathParts, body) -> Maybe (AppCtx returnT)
   route _ handler (method, xs, z) = if "POST" == method then route (Proxy :: Proxy s) handler (method, xs, z) else Nothing
 
-instance (KnownSymbol s, HasServer r x z) => HasServer ((s :: Symbol) :> r) x z where
-  route :: Proxy (s :> r) -> Server r -> (T.Text, [T.Text], z) -> Maybe (AppCtx x)
+instance (KnownSymbol s, HasServer r returnT body) => HasServer ((s :: Symbol) :> r) returnT body where
+  route :: Proxy (s :> r) -> Server r -> (Method, PathParts, body) -> Maybe (AppCtx returnT)
   route _ handler (method, x : xs, z) = if T.pack (symbolVal (Proxy :: Proxy s)) == x then route (Proxy :: Proxy r) handler (method, xs, z) else Nothing
   route _ _ _ = Nothing
 
-instance (Show a, Read a, HasServer r x z) => HasServer (Capture a :> r) x z where
-  route :: Proxy (Capture a :> r) -> (a -> Server r) -> (T.Text, [T.Text], z) -> Maybe (AppCtx x)
+instance (Read a, HasServer r returnT body) => HasServer (Capture a :> r) returnT body where
+  route :: Proxy (Capture a :> r) -> (a -> Server r) -> (Method, PathParts, body) -> Maybe (AppCtx returnT)
   route _ handler (method, x : xs, z) = readMaybe (T.unpack x) >>= \a -> route (Proxy :: Proxy r) (handler a) (method, xs, z)
   route _ _ _ = Nothing
 
-instance (FromJSON a, HasServer r x ByteString, HasServer r x a) => HasServer (BodyParser a :> r) x ByteString where
-  route :: Proxy (BodyParser a :> r) -> (a -> Server r) -> (T.Text, [T.Text], ByteString) -> Maybe (AppCtx x)
+instance (FromJSON parsedBodyT, HasServer r returnT ByteString, HasServer r returnT parsedBodyT) => HasServer (BodyParser parsedBodyT :> r) returnT ByteString where
+  route :: Proxy (BodyParser parsedBodyT :> r) -> (parsedBodyT -> Server r) -> (Method, PathParts, ByteString) -> Maybe (AppCtx returnT)
   route _ handler (method, xs, z) =
     case parseBody z of
       Left e -> Just (throwError e)
